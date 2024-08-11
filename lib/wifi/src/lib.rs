b04595a20296ba020bf10f2e49a9c2e47c3c7125
Vasily Khoruzhick <anarsoul@gmail.com>
2024-08-10 20:41:30 -0700

    More formatting fixes

--- lib/wifi/src/lib.rs
+++ lib/wifi/src/lib.rs
@@ -1,8 +1,8 @@
 use anyhow::{bail, Result};
 use esp_idf_svc::{
     eventloop::EspSystemEventLoop,
-    nvs::EspDefaultNvsPartition,
     hal::peripheral,
+    nvs::EspDefaultNvsPartition,
     wifi::{AuthMethod, BlockingWifi, ClientConfiguration, Configuration, EspWifi},
 };
 use log::info;
